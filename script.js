f0975ac93b82a8b6f2580c0a3baab51ed021e904
isabellozano37 <isabeloficial37@gmail.com>
2023-06-16 12:02:10 +0200

    changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var contentContainer = document.getElementById("content");
 
 function loadPage(url) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       contentContainer.innerHTML = this.responseText;
     }
@@ -13,9 +13,47 @@ function loadPage(url) {
 }
 
 for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", function(event) {
-    event.preventDefault(); 
+  navLinks[i].addEventListener("click", function (event) {
+    event.preventDefault();
     var url = this.href;
     loadPage(url);
   });
 }
+
+function validateForm() {
+
+  let emailInput = document.getElementById("email");
+  let emailValue = emailInput.value.trim();
+ 
+  return validForm;
+}
+
+function checkPasswords() {
+  let passwordInput = document.getElementById("password");
+  let passwordValue = passwordInput.value.trim();
+  let confirmPasswordInput = document.getElementById("confirmatePassword");
+  let confirmPasswordValue = confirmPasswordInput.value.trim();
+
+  if (passwordValue !== confirmPasswordValue) {
+    let passwordError = document.getElementById("password-error");
+    passwordError.textContent = "Las contraseñas no coinciden";
+    passwordError.style.display = "inline-block";
+  } else {
+    let passwordError = document.getElementById("password-error");
+    passwordError.style.display = "none";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("showFormButton").addEventListener("click", function () {
+    var formContainer = document.getElementById("additionalFormContainer");
+    formContainer.style.display = "block";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("loginButton").addEventListener("click", function () {
+    var formContainer = document.getElementById("FormContainer");
+    formContainer.style.display = "block";
+  });
+});
