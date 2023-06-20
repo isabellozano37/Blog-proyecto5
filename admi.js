38b73d509932367df1555be0512c676548a154e7
isabellozano37 <isabeloficial37@gmail.com>
2023-06-20 12:29:17 +0200

    changes

--- admi.js
+++ admi.js
@@ -12,36 +12,72 @@ document.addEventListener("DOMContentLoaded", function () {
     const content = contentInput.value;
     const photo = photoInput.files[0];
 
+    const reader = new FileReader();
+    reader.onload = function () {
+      const photoDataUrl = reader.result;
+
+      const entryElement = createEntryElement(title, content, photoDataUrl);
+      entryList.appendChild(entryElement);
+
+      titleInput.value = '';
+      contentInput.value = '';
+      photoInput.value = '';
+
+      saveEntriesToStorage();
+    };
+
+    if (photo) {
+      reader.readAsDataURL(photo);
+    } else {
+      reader.onload();
+    }
+  }
+
+  function createEntryElement(title, content, photoSrc) {
     const entryElement = document.createElement('div');
-    const titleElement = document.createElement('h3');
-    titleElement.textContent = title;
-    const contentElement = document.createElement('p');
-    contentElement.textContent = content;
+    entryElement.innerHTML = `
+      <h3>${title}</h3>
+      <p>${content}</p>
+    `;
+
+    if (photoSrc) {
+      const photoElement = document.createElement('img');
+      photoElement.classList.add('entry-image');
+      photoElement.src = photoSrc;
+      entryElement.appendChild(photoElement);
+    }
+
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Eliminar';
     deleteButton.addEventListener('click', function () {
       entryList.removeChild(entryElement);
+      saveEntriesToStorage();
     });
-
-    entryElement.appendChild(titleElement);
-    entryElement.appendChild(contentElement);
-
-
-    if (photo) {
-      const photoElement = document.createElement('img');
-      photoElement.classList.add('entry-image');
-      photoElement.src = URL.createObjectURL(photo);
-      entryElement.appendChild(photoElement);
-    }
-
     entryElement.appendChild(deleteButton);
 
-    entryList.appendChild(entryElement);
+    return entryElement;
+  }
 
-    titleInput.value = '';
-    contentInput.value = '';
-    photoInput.value = '';
+  function saveEntriesToStorage() {
+    const entries = Array.from(entryList.children).map(function (entryElement) {
+      const title = entryElement.querySelector('h3').textContent;
+      const content = entryElement.querySelector('p').textContent;
+      const photoSrc = entryElement.querySelector('img')?.src || '';
+      return { title, content, photoSrc };
+    });
+
+    localStorage.setItem('entries', JSON.stringify(entries));
+  }
+
+  function loadEntriesFromStorage() {
+    const entries = JSON.parse(localStorage.getItem('entries') || '[]');
+
+    entries.forEach(function (entry) {
+      const entryElement = createEntryElement(entry.title, entry.content, entry.photoSrc);
+      entryList.appendChild(entryElement);
+    });
   }
 
   entryForm.addEventListener('submit', handleFormSubmit);
+  loadEntriesFromStorage();
 });
